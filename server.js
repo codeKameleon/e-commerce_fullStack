ca6c3c3e25935714a04c5b18c243f887f971e831
axel <axel@pop-os.localdomain>
2022-01-14 11:21:00 +0100

    fix server issues + create router

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
+const router  = require('./routes/api')
 const app = express()
-app.use(express.json())
 
 const dbURI = "mongodb+srv://codeKameleon:mongogo42@cluster0.flrvf.mongodb.net/fake-store?retryWrites=true&w=majority"
-const port = process.env.PORT || 4000
+const port = 4000
 
 mongoose.connect(dbURI)
 .then((result) => {
-    app.listen(port)
+    app.listen(port, "localhost", () => console.log('server started'))
     console.log('db connected')
 })
-.catch((err) => console.log(err));
+.catch((err) => console.log(err))
+
+//initialize routes
+app.use('/api', router)
+
+app.use(express.json())
+
+//error handling middleware
+app.use(function(err,req,res,next){
+    console.log(err);
+    res.status(422).send({error: err.message})
+});
