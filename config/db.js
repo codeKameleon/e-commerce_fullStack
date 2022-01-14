868084c0ad3d40b26605b46d47e9a423fc692b13
axel <axel@pop-os.localdomain>
2022-01-14 18:35:33 +0100

    create schema and instanciate model for product collection

--- config/db.js
+++ config/db.js
@@ -5,12 +5,10 @@ const mongoose = require('mongoose')
 const app = express()
 
 const dbURI = process.env.DB_URI
-const port = process.env.PORT || 4000
 
 const connectDB = async() => {
     try {
         await mongoose.connect(dbURI)
-        app.listen(port, "localhost", () => console.log('server started'))
         console.log('db connected')
     } catch(error) {
         console.log("MongoDB connection FAIL")
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
-const connectDB = require('./config/db');
+const connectDB = require('./config/db')
 const router  = require('./routes/api')
 
 const app = express()
+
+const port = process.env.PORT || 4000
 
 //db connection
 connectDB()
@@ -18,3 +20,5 @@ app.use(function(err,req,res,next){
     console.log(err);
     res.status(422).send({error: err.message})
 });
+
+app.listen(port, "localhost", () => console.log('server started'))
