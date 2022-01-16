16a3337a7aa0ceb262ba6385eb44bd18c14908c9
axel <axel@pop-os.localdomain>
2022-01-16 17:21:15 +0100

    update authController - add check if user already exist on registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,6 +8,10 @@ const createNewUser = async(req, res) => {
     const  { error } = registerValidation(req.body)
     if(error) return res.status(400).send({ error: error.details[0].message })
 
+    // CHECK IF USER ALREADY EXISTS
+    const registeringUser = UserModel.findOne({ email: req.body.email })
+    if(registeringUser) return res.status(400).send({ message: "Email or password already exists"})
+
     // CRYPT PASSWORD
     const salt = await bcrypt.genSalt()
     const hashPassword = await bcrypt.hash(req.body.password, salt)
@@ -20,7 +24,7 @@ const createNewUser = async(req, res) => {
         password: hashPassword,
         phone: req.body.phone
     })
-    
+
     try {
         const newUser = await user.save()
         res.send(newUser)
